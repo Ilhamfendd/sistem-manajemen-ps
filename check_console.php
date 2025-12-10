3b39264eec2baae87c0566159be4d28f7892aeb2
Ilhamorion <ismaililham066@gmail.com>
2025-12-10 18:23:04 +0700

    Clean: Remove duplicate controllers and debug files

--- check_console.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli('localhost', 'root', '', 'ps_manager');
-$result = $conn->query('SELECT id, console_name, console_type, price_per_hour FROM consoles LIMIT 1');
-$row = $result->fetch_assoc();
-echo "console_type value: '" . $row['console_type'] . "'\n";
-echo "console_type length: " . strlen($row['console_type']) . "\n";
-var_dump($row);
-?>
